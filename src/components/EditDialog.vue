309558c53dbd4778df923abb6edb2e16f3b89d5f
Arianna Masciolini <uzkamascio@gmail.com>
2025-11-22 17:31:34 +0100

    single reusable dialog (breaks edge editor?)

--- src/components/EditDialog.vue
+++ src/components/EditDialog.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="dialog" style="display: inline-block">
-    <label for="">New upos : </label>
+    <label for="">New value: </label>
     <input
       ref="input"
       type="text"
@@ -12,18 +12,20 @@
 
 <script>
 export default {
+  name: "EditDialog",
   props: ["sentenceBus"],
   data() {
     return {
       dialog: false,
       tagValue: "",
-      token: {}
+      params: {}
     };
   },
   mounted() {
-    this.sentenceBus.$on("open:uposDialog", token => {
+    this.sentenceBus.$on("open:editDialog", params => {
       this.dialog = true;
-      this.token = token;
+      this.token = params;
+      this.field = params["FIELD"];
       this.focusInput();
     });
     this.sentenceBus.$on("reset:allDialog", () => {
@@ -33,11 +35,21 @@ export default {
   },
   methods: {
     onValidate() {
-      let newUpos = this.tagValue;
-      if (newUpos === "") {
-        newUpos = "X";
+      let newValue = this.tagValue;
+      if (newValue === "") {
+        newValue = "_";
       }
-      this.token.UPOS = newUpos.toUpperCase();
+      switch (this.field) {
+        case "UPOS":
+            this.token.UPOS = newValue;
+            break;
+        case "DEPREL":
+            this.token.DEPREL = newValue;
+            break;
+        default:
+            alert("Ooops! We can't change that yet!")
+            break;
+      }
       this.sentenceBus.$emit("update:token", this.token);
       this.token = {};
       this.dialog = false;
